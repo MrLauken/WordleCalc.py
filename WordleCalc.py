7d3f98590c95da904209309ba5b9179bea3394ec
Remember remember the fifth of november <Henrika2001@hotmail.com>
2022-04-10 17:08:34 +0200

    Beta 2.0
    Wordle søker nå også igjennom bokstaver med ukjent plasserin.
    Known issues: Scriptet tar ikke stilling til hvorvidt de siste elementene er None-types eller ikke i ekstraliste

--- WordleCalc.py
+++ WordleCalc.py
@@ -9,8 +9,8 @@ def DO_something():
 emptystring= ""
 d = enchant.Dict("en_US")
 Liste = ["A","B","C","D","E","F","G","H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","v","W", "X", "Y", "Z"]
+ekstraliste = [None, None, None, None, None]
 ordliste=[]
-ekstraliste=[]
 teller=0
 for x in range(1,6):
     yes_NO= input("\n Do you know letter nr. " + str(x) +". in the word? (y/n): ")
@@ -25,12 +25,21 @@ for x in range(1,6):
             time.sleep(1)
         sys.exit()
 
-yes_NO=str(input("Are there any letters of unknown placement? (y/n): ")).upper()
-
+yes_NO=str(input("\n Are there any letters of unknown placement? (y/n): ")).upper()
+k=1
 if yes_NO=="Y":
-    ekstrabok = str(input("Please write letter/letters: (E.G POERTY) ").upper())
-    for x in ekstrabok:
-        ekstraliste.append(x)
+    while yes_NO == "Y" and k<<6 :
+        ekstrabok = str(input("\n Please write the " + str(k) + ". letter of unknown placement: ")).upper()
+        plassering = input("\n Where can the letter still be placed in the word? : (e.g 1345)  ")
+        for x in plassering: 
+            if ekstraliste[int(x)-1]!= None:
+                ekstraliste[int(x)-1] = str(ekstraliste[int(x)-1]) + str(ekstrabok)
+            else:
+                ekstraliste[int(x)-1] =  ekstrabok
+        yes_NO=str(input("\n Are there any more letters of unknown placement? (y/n): ")).upper()
+        k+=1
+elif yes_NO=="N":
+    print()
 else:
         for x in range(0,16):
             print("\n ERROR: Invalid input, script will autoterminate in 15 seconds")
@@ -46,46 +55,66 @@ for x in str(excluded_letters.upper()):
 p=0
 z=0
 q=0
+l = 0
 ordliste1=[]
 ekstraliste1=[]
 godkjenteord = []
 Sigma= True
-teller=5-teller
-
+teller+=k-1
 for x in ordliste:
     ordliste1.append(x)
 
-
 while p<=100 and z<=150000:
     aight=1
-    if len(ekstraliste)>>0:
-        for x in ekstraliste:
-            ekstraliste1.append(x)
+    ekstraliste1=[]
+    s=0
+    for x in ekstraliste:
+        ekstraliste1.append(x)
+        if x != None:
+            s+=1
     for x in ordliste:
         if x== None:
-            if teller == 0 or len(ekstraliste1)==0:
-                ordliste1[q] = Liste[random.randint(0,len(Liste)-1)]
-                aight+=1
-            elif teller == aight:
-                ordliste1[q]= ekstraliste[random.randint(0,len(ekstraliste)-1)]
-                ekstraliste1.remove(ordliste1[q])
-                aight+=1
-            else: 
-                randa =(teller-len(ekstraliste))*100/(teller) 
-                check = random.randint(0,100)
-                if check>=int(randa):
+            if ekstraliste[q]!=None and ekstraliste1[q]!="":
+                sjanse = random.randint(0,len(ekstraliste1[q])+1)
+                #print(int(teller+l), sjanse,ekstraliste1, ekstraliste1[q], "print 1")
+                if sjanse>=1 or teller+l>=5: 
+                    p = 0
+                    sjanse = random.randint(0,int(len(ekstraliste1[q]))-1)   
+                    for x in ekstraliste1[q]:
+                        
+                        #print(sjanse, p, x)
+                        if sjanse == p:
+                            #print(x, "Print 2")
+                            ordliste1[q] = x
+                            tall = 0
+                            streng = ""
+                            for w in ekstraliste1:
+                                streng = str(w)
+                                if w!= None:
+                                    for y in w:
+                                        if y == x:
+                                            streng = streng.replace(x, "")
+                                ekstraliste1[tall] = streng
+                                streng = ""
+                                tall+=1 
+                            break
+                        p+=1
+                        
+                else: 
                     ordliste1[q] = Liste[random.randint(0,len(Liste)-1)]
-                    aight+=1
-                else:
-                    ordliste1[q]= ekstraliste[random.randint(0,len(ekstraliste)-1)]
-                    ekstraliste1.remove(ordliste1[q])
-                    aight+=1
-
+                    l+=1
+                    #print(ordliste1[q], "print 3")
+            else: 
+                ordliste1[q] = Liste[random.randint(0,len(Liste)-1)]
+                l+=1
+                #print(ordliste1[q], "print 4")
                 
         q+=1
     q=0
+    #print(ordliste1, "print 5")
     strin = "".join(ordliste1)    
-    
+    l=0
+
 
     if d.check(strin):
         for x in godkjenteord:
@@ -98,13 +127,14 @@ while p<=100 and z<=150000:
             p+=1
         Sigma=True
     z+=1
+   
 
 if p>=100:
-    print("Program ended early due to too many possible words")         
+    print("\nProgram ended early due to too many possible words")         
 else:
-    print("The great search for words terminated correctly due to runtime. This limitation might have caused certain words to escape, although this is fairly unlikely ")
+    print("\nThe great search for words terminated correctly due to runtime. This limitation might have caused certain words to escape, although this is fairly unlikely ")
 
-print("Script will autoterminate in 60 seconds")
+print("\nScript will autoterminate in 60 seconds")
 time.sleep(60)
 
     
